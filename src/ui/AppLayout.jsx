269ee1df6b60aaa05c27da48438d3c2e6b3c101b
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2024-03-01 15:38:51 +0200

    tailwindCssStyling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,19 +1,21 @@
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 
 function AppLayout() {
   const navigation = useNavigation();
   console.log(navigation);
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   return (
-    <div className="layout">
+    <div className="grid  h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="my-10 overflow-scroll">
+        <main className="mx-auto max-w-3xl ">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
