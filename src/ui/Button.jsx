f3ae0a312f7bd91819026c4fb82a2371bb208dd4
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2024-03-05 12:28:54 +0200

    ending styling

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to = '', type = 'primary' }) {
+function Button({ children, disabled, to = '', type }) {
   const base =
     'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ';
 
@@ -11,6 +11,7 @@ function Button({ children, disabled, to = '', type = 'primary' }) {
       uppercase tracking-wide text-stone-400 transition-colors duration-300 focus:text-stone-800
       hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring hover:text-stone-800
       focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 sm:px-6 sm:py-3.5`,
+    large: base + `px-6 py-4 text-xl font bold`,
   };
   if (to !== '') {
     return (
