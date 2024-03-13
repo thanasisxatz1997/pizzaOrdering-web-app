9c74064fc1df15c3028db191a6a99ab29c1a3e71
thanasisxatz1997 <93034841+thanasisxatz1997@users.noreply.github.com>
2024-03-13 12:24:19 +0200

    useFetcher

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,7 +1,8 @@
 // Test ID: IIDSAT
 import { getOrder } from '../../services/apiRestaurant';
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import OrderItem from './OrderItem';
+import { useEffect } from 'react';
 
 import {
   calcMinutesLeft,
@@ -13,6 +14,11 @@ function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const order = useLoaderData();
 
+  const fetcher = useFetcher();
+
+  useEffect(function () {
+    if (!fetcher.data) fetcher.load('/menu');
+  }, []);
   const {
     id,
     status,
